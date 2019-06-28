fab7116723c793e454c9e51121e4d84a765d7eae
Darin Armstrong <darin.marmstrong@gmail.com>
2019-06-28 10:23:10 -0600

    Setup View and Custom Table View Cell

---
+++ MovieSearch/Views/MovieTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieTableViewCell.swift
+//  MovieSearch
+//
+//  Created by Darin Marcus Armstrong on 6/28/19.
+//  Copyright © 2019 Darin Marcus Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var movieRatingLabel: UILabel!
+    @IBOutlet weak var movieOverviewLabel: UILabel!
+    
+}
